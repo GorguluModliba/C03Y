de1d132bdf53ca01470927577dc13f2dc135e18f
ygoerguelue <ygoerguelue@student.tgm.ac.at>
2017-03-13 11:00:50 +0100

    V01

---
+++ src/goerguelue/HelloWorldygoerguelue.java
@@ -0,0 +1,12 @@
+package goerguelue;
+
+public class HelloWorldygoerguelue {
+
+	public static void HelloWorld(){
+		System.out.println("Schöne Grüße von Yasin Görgülü.");
+	}
+
+	public static void main(String[] args){
+		HelloWorld();
+	} 
+}
